4197c06a0cb1d179e150acf36c93a8d2f70f0d1e
stan-dot <stanislawmalinowski09@gmail.com>
2022-07-16 20:59:45 +0100

    added more packages and swagger

--- backend/src/app.module.ts
+++ backend/src/app.module.ts
@@ -1,10 +1,16 @@
 import { Module } from '@nestjs/common';
 import { ConfigModule } from '@nestjs/config';
+import { MulterModule } from '@nestjs/platform-express';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
 
 @Module({
-  imports: [ConfigModule.forRoot()],
+  imports: [
+    ConfigModule.forRoot(),
+    MulterModule.register({
+      dest: '../upload',
+    }),
+  ],
   controllers: [AppController],
   providers: [AppService],
 })
--- backend/src/main.ts
+++ backend/src/main.ts
@@ -1,11 +1,20 @@
 import { NestFactory } from '@nestjs/core';
+import { DocumentBuilder } from '@nestjs/swagger/dist/document-builder';
+import { SwaggerModule } from '@nestjs/swagger/dist/swagger-module';
 import { AppModule } from './app.module';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
   const port = 3000;
   // add swagger
-  
+  const swaggerOptions = new DocumentBuilder()
+    .setTitle('Lesson 16 Project')
+    .setVersion('1.0.0')
+    .setDescription('Encode Club Bootcamp June Project for Lesson 16')
+    .build();
+  const document = SwaggerModule.createDocument(app, swaggerOptions);
+  SwaggerModule.setup('docs', app, document);
+
   await app.listen(port);
 }
 bootstrap();
